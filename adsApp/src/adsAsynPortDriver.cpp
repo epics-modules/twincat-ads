epics-modules/twincat-ads

commit 9a01fcc8767c7830aff48bb23f98aa534707ee9e

Fix confusing bulk read time printf in adsAsynPortDriver
Érico Nogueira Rolim points out that the printout about the
bulk read time is confusing.
Simplify the printout: Remove the confusing
"Default Sample Time of %d ms is too small"
and print only the resulting time (which is 1000msec or more)

adsApp/src/adsAsynPortDriver.cpp
@@ -414,13 +414,11 @@ adsAsynPortDriver::adsAsynPortDriver(const char *portName, const char *ipaddr,
     bulk[i].cnt = 0; // Entry is currently unused!!
   bulkTScnt = 0;
   if (defaultSampleTimeMS_ < 1000) {
-    printf("Default Sample Time of %d ms is too small, defaulting to 1Hz.\n",
-           defaultSampleTimeMS_);
     bulk_delay_us = 1000000; // 1 Hz
   } else {
-    printf("Default bulk read time: %d ms\n", defaultSampleTimeMS_);
     bulk_delay_us = defaultSampleTimeMS_ * 1000;
   }
+  printf("bulk read time: %d ms\n", defaultSampleTimeMS_);
   bulkdatasize = 4 * 1024 * 1024; // This is excessive!
   bulkdata = (uint8_t *)malloc(bulkdatasize);
   bulkOK = 0;

commit 5582a3eb8837115c5662ce225ca65dc1b39bbe7c

Fix wrong value of reported bulk read time
We set bulk_delay_us to a default value or based on
defaultSampleTimeMS_. Make sure the message printed to the terminal is
always correct by printing the value of bulk_delay_us in ms.

Fixes: 9a01fcc (Fix confusing bulk read time printf in
adsAsynPortDriver, 2026-05-22)

adsApp/src/adsAsynPortDriver.cpp
@@ -418,7 +418,7 @@ adsAsynPortDriver::adsAsynPortDriver(const char *portName, const char *ipaddr,
   } else {
     bulk_delay_us = defaultSampleTimeMS_ * 1000;
   }
-  printf("bulk read time: %d ms\n", defaultSampleTimeMS_);
+  printf("bulk read time: %d ms\n", bulk_delay_us / 1000);
   bulkdatasize = 4 * 1024 * 1024; // This is excessive!
   bulkdata = (uint8_t *)malloc(bulkdatasize);
   bulkOK = 0;